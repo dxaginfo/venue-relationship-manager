53541c2d2bbc09f9017a8818f87720843a03b282
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-02 17:48:00 -0300

    Add basic React App.js scaffold.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Venue Relationship Tracker</h1>
+      <p>Welcome to the venue relationship tracker. Features coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
